b2068f8b41c66343b8196b13eac780a57e8711dc
jantimmer91 <jan.timmer@hotmail.de>
2022-01-21 18:41:45 +0100

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+export const routes = [
+    {
+        title: 'Meine Reservierungen',
+        path: '/reservierungshistorie',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Verfügbarkeit einsehen',
+        path: '/scooterStatus',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Scooter reservieren',
+        path: '/',
+        cName: 'dropdown-link'
+    },
+];
